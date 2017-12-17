9e9a9c3f4b327ea8679e0105c969aa96c30f2b61
jennifer <jgonzal10@gmail.com>
2017-12-17 15:51:13 -0200

    '18correction'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,17 @@ import { FormsModule } from '@angular/forms';
 import { Http, HttpModule } from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+//import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+//import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthGuard } from './shared';
 // AoT requires an exported function for factories
-export function HttpLoaderFactory(http: Http) {
+//export function HttpLoaderFactory(http: Http) {
 //     for development
 //     return new TranslateHttpLoader(http, '/start-angular/SB-Admin-BS4-Angular-4/master/dist/assets/i18n/', '.json');
-    return new TranslateHttpLoader(http, 'dist/assets/i18n/', '.json');
-}
+ //   return new TranslateHttpLoader(http, '/dist/assets/i18n/', '.json');
+//}
 @NgModule({
     declarations: [
         AppComponent
@@ -24,13 +24,13 @@ export function HttpLoaderFactory(http: Http) {
         FormsModule,
         HttpModule,
         AppRoutingModule,
-        TranslateModule.forRoot({
+      /*  TranslateModule.forRoot({
             loader: {
                 provide: TranslateLoader,
                 useFactory: HttpLoaderFactory,
                 deps: [Http]
             }
-        })
+        })*/
     ],
     providers: [AuthGuard],
     bootstrap: [AppComponent]
--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
-import { TranslateService } from '@ngx-translate/core';
+//import { TranslateService } from '@ngx-translate/core';
 
 @Component({
     selector: 'app-header',
@@ -9,7 +9,7 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class HeaderComponent implements OnInit {
 
-    constructor(private translate: TranslateService, public router: Router) {
+    constructor( public router: Router) {
         this.router.events.subscribe((val) => {
             if (val instanceof NavigationEnd && window.innerWidth <= 992) {
                 this.toggleSidebar();
@@ -34,6 +34,6 @@ export class HeaderComponent implements OnInit {
     }
 
     changeLang(language: string) {
-        this.translate.use(language);
+      //  this.translate.use(language);
     }
 }
